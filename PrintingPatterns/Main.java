fc5746ade89238487b066d0b9cf15878b0a7642b
vinitadeep <vdvinitadeep15@gmail.com>
2025-05-30 21:40:06 +0530

    Day1 : Pattern Printing Practice 30/05

---
+++ PrintingPatterns/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        PatternPrinter.printSquarePattern();
+        PatternPrinter.printHollowSquarePattern();
+        PatternPrinter.printRectanglePattern();
+        PatternPrinter.printHollowRectanglePattern();
+        PatternPrinter.printRhombus();
+        PatternPrinter.printParallelogram();
+        PatternPrinter.printMirroredRhombus();
+    }
+}
