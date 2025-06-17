0c58ebc2a57c2462d0f8683324c07da24ab56b0a
vinitadeep <vdvinitadeep15@gmail.com>
2025-06-17 16:09:36 +0530

    Day1 : Pattern Printing Practice 30/05

--- PrintingPatterns/PatternPrinter.java
+++ PrintingPatterns/PatternPrinter.java
@@ -98,4 +98,70 @@ public class PatternPrinter {
         }
         System.out.print('\n');
     }
+
+    public static void printTriangleStar(){
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        System.out.print('\n');
+    }
+
+    public static void printMirrorTriangleStar(){
+        for(int i = 0; i < 4; i++){
+            for(int j = i; j < 4; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        System.out.print('\n');
+    }
+
+    public static void printHalfDiamond(){
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        for(int i = 0; i < 3; i++){
+            for(int j = i; j < 3; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        System.out.print('\n');
+    }
+
+    public static void printPyramid(){
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5-1-i; j++){
+                System.out.print(' ');
+            }
+            for(int k = 1; k <= (2 * i + 1); k++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        System.out.print('\n');
+    }
+
+    public static void printHollowPyramid(){
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5-1-i; j++){
+                System.out.print(' ');
+            }
+            for (int k = 0; k < (2 * i + 1); k++) {
+                if (k == 0 || k == (2 * i) || i == 5 - 1) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+        System.out.print('\n');
+    }
 }
